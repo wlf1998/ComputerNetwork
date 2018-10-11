bcc088e9efaa7c07d174673f43a64f60fb128c7b
drdh <drdh@mail.ustc.edu.cn>
2018-10-11 16:39:48 +0800

    [DNS server]custom ip

--- DNS-Server/README.md
+++ DNS-Server/README.md
@@ -1,6 +1,14 @@
-`ip link`查看本机的ip地址`114.214.176.57`
+`ip link`查看本机的ip地址`114.214.176.57`每次不一样
 
 编辑`/etc/resolv.conf`使DNS服务器为上面的地址
 
 使用`create_ap`创建热点`sudo create_ap wlp9s0 enp0s20f0u3 Arch archwifi`在另一个电脑上连接，然后修改DNS服务器为上面的ip地址，就可以正常查询了
+
+
+
+[DNS 介绍与粗实现](http://www.cnblogs.com/dongkuo/p/6714071.html); [相关代码](https://github.com/dongkuo/dns-server)
+
+[DNS 使用dnslib案例](http://www.isnowfy.com/introduction-to-gevent/); [相关代码](https://github.com/isnowfy/dns)
+
+[dnslib 介绍](https://bitbucket.org/paulc/dnslib/)
 
--- DNS-Server/src/test.py
+++ DNS-Server/src/test.py
@@ -1,17 +1,11 @@
 import threading
 import socket
 import socketserver
-from dnslib import DNSRecord
+from dnslib import DNSRecord,QTYPE
 import dnslib
 
-
 class Handler(socketserver.BaseRequestHandler):    
-    def handle(self):
-        #self.cache={}
-        # 外部添加
-        
-        cache["abc.com."]="1.2.3.4"
-        
+    def handle(self):        
         request_data = self.request[0]
         client_socket = self.request[1]
         #内部搜索
@@ -22,15 +16,18 @@ class Handler(socketserver.BaseRequestHandler):
         print(qname)
         if search:
             ret=d.reply()
-            ret.add_answer(dnslib.RR(qname,rdata=dnslib.A(search)))
-            #ret.add_answer(dnslib.RR(qname,rdata=dnslib.A("1.2.3.5")))
+            # 不良网站
+            if search=="0.0.0.0":
+                ret.add_answer(dnslib.RR(qname,QTYPE.TXT,rdata=dnslib.TXT(warning)))
+            else:
+                ret.add_answer(dnslib.RR(qname,rdata=dnslib.A(search)))
             ret.header.id=qid
             client_socket.sendto(bytes(ret.pack()), self.client_address)
         else:
             # 将请求转发到 外部 DNS
             redirect_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             redirect_socket.sendto(request_data, ('202.38.64.56', 53))
-            response_data, address = redirect_socket.recvfrom(1024)
+            response_data, address = redirect_socket.recvfrom(8192)
             # 将外部响应响应给客户
             client_socket.sendto(response_data, self.client_address)
 
@@ -40,12 +37,29 @@ class Server(socketserver.ThreadingMixIn, socketserver.UDPServer):
 
 
 if __name__ == "__main__":
+    # 添加外部的ip地址
+    f=open("./ip.txt",'r')
+    ip=[]
+    for a in f.readlines():
+        if len(a)>=2:
+            ip.append(a.strip().split(" "))
+    
     cache={}
+    for i in ip:
+        cache[i[1]+'.']=i[0]
+
+    cache["abc.com."]="1.2.3.4"
+    cache["wrong.com."]="0.0.0.0"
+    cache["drdh.com."]="185.199.109.153"
+    # 不良网站警告信息
+    warning="illegal website"
+   
     # ip需换成自己电脑的ip
-    server = Server(('114.214.176.57', 53), Handler)
+    localIP="114.214.175.45"
+    server = Server((localIP, 53), Handler)
     with server:
         server_thread = threading.Thread(target=server.serve_forever)
         server_thread.daemon = True
         server_thread.start()
-        print('The DNS server is running at 114.214.176.57 ...')
+        print('The DNS server is running at %s ...',localIP)
         server_thread.join()
